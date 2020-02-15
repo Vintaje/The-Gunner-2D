2e88adf44e9373d130655357ee2e235d0667045b
jorgedam96 <48628681+jorgedam96@users.noreply.github.com>
2020-02-15 20:18:05 +0100

    equals

--- Assets/Scripts/HorizontalMovePlatform.cs
+++ Assets/Scripts/HorizontalMovePlatform.cs
@@ -6,17 +6,24 @@ public class HorizontalMovePlatform : MonoBehaviour
 {
     private float speedUpDown = 0.33f;
     private float distanceUpDown = 2;
+    private Vector3 mov;
+
+    public Vector3 Mov
+    {
+        get { return mov; }
+        set { mov = value; }
+    }
+    
 
     private void Start()
     {
     }
     void Update()
     {
-        Vector3 mov = new Vector3(Mathf.Sin(speedUpDown * Time.time) * distanceUpDown+8f, transform.position.y, transform.position.z);
+        mov = new Vector3(Mathf.Sin(speedUpDown * Time.time) * distanceUpDown + 8f, transform.position.y, transform.position.z);
         transform.position = mov;
+
     }
 
-    private void OnCollisionEnter2D(Collision2D other) {
-        
-    }
+   
 }
