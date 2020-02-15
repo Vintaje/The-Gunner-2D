cc79a346f5bb7a81afa14fb03deaf0e9c1cf3fe3
jorgedam96 <48628681+jorgedam96@users.noreply.github.com>
2020-02-15 20:00:19 +0100

    plataforma en movimiento con seno

---
+++ Assets/Scripts/HorizontalMovePlatform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalMovePlatform : MonoBehaviour
+{
+    private float speedUpDown = 0.33f;
+    private float distanceUpDown = 2;
+
+    private void Start()
+    {
+    }
+    void Update()
+    {
+        Vector3 mov = new Vector3(Mathf.Sin(speedUpDown * Time.time) * distanceUpDown+8f, transform.position.y, transform.position.z);
+        transform.position = mov;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        
+    }
+}
