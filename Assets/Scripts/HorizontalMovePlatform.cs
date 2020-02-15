8cb4dcf08dcf3474e7e0c095502f44e7cb589590
jorgedam96 <48628681+jorgedam96@users.noreply.github.com>
2020-02-15 20:43:31 +0100

    Update HorizontalMovePlatform.cs

--- Assets/Scripts/HorizontalMovePlatform.cs
+++ Assets/Scripts/HorizontalMovePlatform.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class HorizontalMovePlatform : MonoBehaviour
 {
-    private float speedUpDown = 0.33f;
+    private float speedUpDown = 1;
     private float distanceUpDown = 2;
     private Vector3 mov;
+    public GameObject player;
 
+    private bool playerAbove = false;
     public Vector3 Mov
     {
         get { return mov; }
         set { mov = value; }
     }
-    
+
 
     private void Start()
     {
@@ -23,7 +25,27 @@ public class HorizontalMovePlatform : MonoBehaviour
         mov = new Vector3(Mathf.Sin(speedUpDown * Time.time) * distanceUpDown + 8f, transform.position.y, transform.position.z);
         transform.position = mov;
 
+        if (playerAbove)
+        {
+            player.transform.position = new Vector3(mov.x, player.transform.position.y,player.transform.position.z);
+        }
+
     }
 
-   
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            playerAbove = false;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            playerAbove = true;
+        }
+    }
+
+
 }
