ceaad191cf7b135a9c4d689770efa78730136ce7
jorgedam96 <48628681+jorgedam96@users.noreply.github.com>
2020-02-20 17:05:03 +0100

    subir escaleras beta

---
+++ Assets/ladder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ladder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag.Equals("Player") && (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)))
+        {
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 2);
+
+        }
+        else if (other.tag.Equals("Player") && (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)))
+        {
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -2);
+        }
+        /*else if(other.tag.Equals("EnemyBullet"))
+        {
+            other.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 1.5f);
+        }*/
+    }
+}
