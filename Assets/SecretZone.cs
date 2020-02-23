c9386ef3790ffc3e327a4d05bba88b47c6efe6e2
jorgedam96 <48628681+jorgedam96@users.noreply.github.com>
2020-02-23 18:59:02 +0100

    Entrada secreta

---
+++ Assets/SecretZone.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class SecretZone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            GameObject.Find("Visual").GetComponent<Tilemap>().color = new Color(1, 1, 1, 0.5f);
+        }
+    }
+
+}
